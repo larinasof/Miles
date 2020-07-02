c393969a92a2cc60b9521aceadb35e47728e87e7
Sofia Larina <zazaq97@gmail.com>
2020-07-02 17:55:55 +0300

    Modified

--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,7 @@
 public class Main {
     public static void main(String[] args){
-        long price = 4599;
-        long bonus = price / 20;
+        int price = 4599;
+        int bonus = price / 20;
         System.out.println(bonus);
     }
 }
